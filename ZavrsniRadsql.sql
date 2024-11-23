8c4464c3cc1f908d1110378b607e379a580dda2c
RobertoKokić <kokic02001@gmail.com>
2024-11-23 09:01:41 +0100

    Update ZavrsniRadsql.sql

--- ZavrsniRadsql.sql
+++ ZavrsniRadsql.sql
@@ -1,30 +1,39 @@
-use master;
-go
+--use master;
+--go
 
-drop database if exist LigaLegendi;
-go
+--drop database if exist LigaLegendi;
+--go
 
-create database LigaLegendi;
-go
+--create database LigaLegendi;
+--go
 
-use database LigaLegendi;
-go
+--use database LigaLegendi;
+--go
 
-create table Legende(
-sifra int not null primary key identity(1,1),
-naziv varchar(50)not null 
-cijena decimal(18,2)null,
-kostimi bit
-);
+--create table Legende(
+--sifra int not null primary key identity(1,1),
+--naziv varchar(50)not null 
+--cijena decimal(18,2)null,
+--kostimi bit
+--);
 
-create table kostimi(
-sifra int not null primary key identity(1,1),
-naziv varchar(50) not null,
-cijena decimal(18,2)null
-);
+--create table kostimi(
+--sifra int not null primary key identity(1,1),
+--naziv varchar(50) not null,
+--cijena decimal(18,2)null
+--);
 
-create table mape(
-sifra int not null primary key identity(1,1),
-naziv varchar(50)not null,
-velicina bit
-);
+--create table mape(
+--sifra int not null primary key identity(1,1),
+--naziv varchar(50)not null,
+--velicina int not null
+--);
+
+insert into Legende (naziv,cijena,kostimi)
+values ('Yasuo',6300,'projektyasuo');
+
+insert into kostimi (naziv,cijena)
+values('project yasuo', 1300);
+
+insert into mape (naziv,velicina)
+values ('summonersrift',null);
