32b32ca2f09c064db88507bd372ddabb4e15dfce
RobertoKokić <kokic02001@gmail.com>
2024-12-04 16:33:29 +0100

    Upoznavanje s sintaksom

--- CSharp/CSHARP/Ucenje/Program.cs
+++ CSharp/CSHARP/Ucenje/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// komentar jedna linija 
+
+/*
+  
+ 
+Komentar vise linija 
+
+*/
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Pozdrav svijetu ćčćč ĆČčć");
+Console.WriteLine("Hello Osijek");
