878b21d0079f7c52495af99168246517fbbb6d51
RobertoKokić <kokic02001@gmail.com>
2024-12-04 18:51:27 +0100

    tidy

---
+++ CSharp/CSHARP/Ucenje/E03Z1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E03Z1
+    {
+     public static void Izvodi()
+    Console.WriteLine("Upiši Broj");
+
+
+    }
+}
