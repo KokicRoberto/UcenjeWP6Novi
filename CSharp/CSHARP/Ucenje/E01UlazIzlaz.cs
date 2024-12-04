3540aa6cb6c84a56fcd6082c3a0b3d3a133023f5
RobertoKokić <kokic02001@gmail.com>
2024-12-04 16:55:22 +0100

    Literatura

---
+++ CSharp/CSHARP/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+    {
+
+     public static void Izvedi()
+       
+       Console.WriteLine("Hello")
+
+       string Ime = Console.ReadLine();
+
+       Console.WriteLine("Ime")
+    }
+}
