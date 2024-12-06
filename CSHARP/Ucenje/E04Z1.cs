d4eb9c0841e3267a41490e77deea7ef587d7432f
RobertoKokić <kokic02001@gmail.com>
2024-12-06 17:53:22 +0100

    E04

---
+++ CSHARP/Ucenje/E04Z1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E04Z1
+    {
+        public static void Izvedi()
+        {
+            //Console.WriteLine("Hello");
+        }
+        Console.Write("Unesite godine");
+        int godine  = int.Parse(Console.ReadLine());
+        if (godine >= 18)
+            {
+            Console.WriteLine("Punoljetan");
+        }
+        else
+       {
+       Console.WriteLine("Maloljetan");
+       }
+
+      
+
+    }
+}
+
