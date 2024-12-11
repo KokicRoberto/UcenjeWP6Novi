a6446ad92ed3da130996b7faac2f0f6c076646b7
RobertoKokić <kokic02001@gmail.com>
2024-12-11 17:04:13 +0100

    Domaca Zadaca 11.12.2024

---
+++ CSHARP/Ucenje/DomacaZadaca.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class DomacaZadaca
+    {
+        public static void Izvedi()
+        {
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Broj redova: 7 ");
+            int brojred1 = 7;
+            Console.WriteLine("Broj stupaca: 7");
+            int brojstup1 = 7;
+            int[,] cTablica1 = new int[brojred1, brojstup1];
+
+            {
+                for (int i = 1; i < brojred1; i++)
+                    for (int j = 0; j < brojstup1; j++)
+                    {
+                        cTablica1[i, j] = i * brojred1 + j;
+                    }
+            }
+
+           
+           
+        }
+    }
+}
